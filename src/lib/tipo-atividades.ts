806f595b9b7e44f175b07a4d6ff0d9c8e7ae5340
lucas-bogos <bogoslucas1@gmail.com>
2023-11-02 18:10:15 -0300

    fix: nenhum parâmetro foi recebido em  na listagem de tipos de atividades

--- src/lib/tipo-atividades.ts
+++ src/lib/tipo-atividades.ts
@@ -16,6 +16,9 @@ export class TipoAtividades extends Api {
   public async listar(
     data: ITipoAtividadeListarRequest
   ): Promise<ITipoAtividadeListarResponse> {
-    return await this.call("/geral/tpativ/", "ListarTipoAtiv", data);
+    return await this.call("/geral/tpativ/", "ListarTipoAtiv", {
+      filtrar_por_codigo: data.filtrar_por_codigo ?? "",
+      filtrar_por_descricao: data.filtrar_por_descricao ?? "",
+    });
   }
 }
